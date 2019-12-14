abb7d5d56bc5b0f5c6c81de8949fbe909ccba54c
michmaml <mjsekulski1@gmail.com>
2019-12-14 20:01:25 +0800

    Game prototype finished, still needs a bit more polish

--- app/src/main/java/com/example/mobilegame/Constants.java
+++ app/src/main/java/com/example/mobilegame/Constants.java
@@ -1,7 +1,10 @@
 package com.example.mobilegame;
 
+import android.content.Context;
+
 public class Constants {
     public static int SCREEN_WIDTH;
     public static int SCREEN_HEIGHT;
+    public static Context CURRENT_CONTEXT;
     public static long INIT_TIME;
 }
--- app/src/main/java/com/example/mobilegame/GameplayScene.java
+++ app/src/main/java/com/example/mobilegame/GameplayScene.java
@@ -16,12 +16,17 @@ public class GameplayScene implements Scene {
     private boolean movingPlayer = false;
     private boolean gameOver = false;
     private long gameOverTime;
+    private OrientationData orientationData;
+    private long frameTime;
 
     public GameplayScene(){
         rplayer = new RectPlayer(new Rect(100, 100, 100, 100), Color.rgb(255, 0, 0));
         playerPoint = new Point(Constants.SCREEN_WIDTH/2, 3*Constants.SCREEN_HEIGHT/4);
         rplayer.update(playerPoint);
         obstacleManager = new ObstacleManager(200, 350, 75, Color.WHITE);
+        orientationData = new OrientationData();
+        orientationData.register();
+        frameTime = System.currentTimeMillis();
     }
 
     public void reset(){
@@ -34,6 +39,30 @@ public class GameplayScene implements Scene {
     @Override
     public void update() {
         if(!gameOver) {
+            if(frameTime < Constants.INIT_TIME)
+                frameTime = Constants.INIT_TIME;
+            int elapsedTime = (int) (System.currentTimeMillis() -frameTime);
+            frameTime = System.currentTimeMillis();
+            if(orientationData.getOrientation() != null && orientationData.getStartOrientation() != null) {
+                float pitch = orientationData.getOrientation()[1] - orientationData.getStartOrientation()[1];
+                float roll = orientationData.getOrientation()[2] - orientationData.getStartOrientation()[2];
+
+                float xSpeed = 2 * roll * Constants.SCREEN_WIDTH/1000f;
+                float ySpeed = pitch * Constants.SCREEN_HEIGHT/1000f;
+
+                playerPoint.x += Math.abs(xSpeed*elapsedTime) > 5 ? xSpeed*elapsedTime : 0;
+                playerPoint.y += Math.abs(ySpeed*elapsedTime) > 5 ? ySpeed*elapsedTime : 0;
+            }
+
+            if(playerPoint.x < 0)
+                playerPoint.x = 0;
+            else if(playerPoint.x > Constants.SCREEN_WIDTH)
+                playerPoint.x = Constants.SCREEN_WIDTH;
+            if(playerPoint.y < 0)
+                playerPoint.y = 0;
+            else if(playerPoint.y > Constants.SCREEN_HEIGHT)
+                playerPoint.y = Constants.SCREEN_HEIGHT;
+
             rplayer.update(playerPoint);
             obstacleManager.update();
             if(obstacleManager.playerCollide(rplayer)){
@@ -70,6 +99,7 @@ public class GameplayScene implements Scene {
                 if(gameOver && System.currentTimeMillis() - gameOverTime >= 2000) {
                     reset();
                     gameOver = false;
+                    orientationData.newGame();
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
